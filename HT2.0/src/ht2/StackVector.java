52f463edf122b490a4fa69a50c6cda53c28af0e9
marcosb11 <marcosb11@laptop>
2015-08-19 23:31:13 -0600

    javadoc generado

--- HT2.0/src/ht2/StackVector.java
+++ HT2.0/src/ht2/StackVector.java
@@ -13,44 +13,76 @@
 
 import java.util.Vector;
 
+/**
+ *
+ * @author marcosb11
+ * @param <E>
+ */
 public class StackVector<E> extends AStack<E>
 {
-	protected Vector<E> data;
 
-	public StackVector()
+    /**
+     *
+     */
+    protected Vector<E> data;
+
+    /**
+     *
+     */
+    public StackVector()
 	// post: constructs a new, empty stack
 	{
 		data = new Vector<E>();
 	}
 
-	public void push(E item)
+    /**
+     *
+     * @param item
+     */
+    public void push(E item)
 	// post: the value is added to the stack
 	//          will be popped next if no intervening push
 	{
 		data.add(item);
 	}
 
-	public E pop()
+    /**
+     *
+     * @return
+     */
+    public E pop()
 	// pre: stack is not empty
 	// post: most recently pushed item is removed and returned
 	{
 		return data.remove(size()-1);
 	}
 
-	public E peek()
+    /**
+     *
+     * @return
+     */
+    public E peek()
 	// pre: stack is not empty
 	// post: top value (next to be popped) is returned
 	{
 		return data.get(size() - 1);
 	}
 	
-	public int size()
+    /**
+     *
+     * @return
+     */
+    public int size()
 	// post: returns the number of elements in the stack
 	{
 		return data.size();
 	}
   
-	public boolean empty()
+    /**
+     *
+     * @return
+     */
+    public boolean empty()
 	// post: returns true if and only if the stack is empty
 	{
 		return size() == 0;
